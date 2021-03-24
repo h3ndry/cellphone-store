8cfdfe3380cb46f22907c6d12981ffc3054c7537
Hendry <h3khoza@gmail.com>
2021-03-24 21:56:51 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2fd62b4cd038af895eadf36519f143d5'),
+    },
+  },
+});
